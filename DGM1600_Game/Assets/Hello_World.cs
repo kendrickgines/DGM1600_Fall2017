cc25027ce6b366ce25c10ac2c0c6bfafd6e7facd
Unknown <kendrickgines@gmail.com>
2017-08-28 20:14:46 -0600

    Test Script 1.0
    Created a test script in Unity

---
+++ DGM1600_Game/Assets/Hello_World.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello_World : MonoBehaviour {
+
+	string message = "Help me!";
+
+	// Use this for initialization
+	void Start () {
+		print("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(message);
+	}
+}
