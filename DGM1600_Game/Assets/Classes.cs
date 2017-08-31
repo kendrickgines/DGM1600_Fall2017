769d033f17cf2e911a4db087e7fc36b515335173
Unknown <kendrickgines@gmail.com>
2017-08-30 20:08:34 -0600

    Created Classes Script
    Three classes that were created for practice.

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+
+		public int money = 1000000;
+		public float fuel = .01f;
+
+		private string name = "Joey";
+
+	}	
+
+	public class Two {
+
+		public bool alive = true;
+
+		public int health = 100;
+
+		public int strength = 10;
+	}
+
+	public class Three {
+
+		public string introMessage = "Hello";
+
+		private string location = "Floor 3";
+
+		private int characterCount = 3;
+	}
+}
