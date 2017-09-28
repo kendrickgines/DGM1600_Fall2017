eefb6bfdc291324cf13e27d9727b5f27626c8a02
Unknown <kendrickgines@gmail.com>
2017-09-27 18:02:58 -0600

    While update

--- DGM1600_Game/Assets/While.cs
+++ DGM1600_Game/Assets/While.cs
@@ -5,14 +5,56 @@ using UnityEngine;
 public class While : MonoBehaviour {
 
 	public int bottles = 100;
+	public int weeks = 52;
+	public int countDown = 30;
+	public int cup = 0;
+
+	public int boat = 100;
+
+	public int gas = 100;
 
 	// Use this for initialization
 	void Start () {
+
+		// While loops can be used to repeat the same action multiple times.  Incrementing the action gives it an endpoint.  
+		// Here are some examples.  
+
 		while(bottles > 0){
 			print(bottles + " bottles of coke on the wall.");
 			bottles--;
 		}
-		print ("No more bottles of coke on the wall.");
+		print("No more bottles of coke on the wall.");
+
+		while(weeks > 0){
+			print(weeks + " weeks left in the year.");
+			weeks--;
+		}
+		print("It's a new year!");
+
+		while(countDown > 0){
+			print(countDown);
+			countDown--;
+		}
+		print("Time's up!");
+
+		while(cup < 100){
+			print("The cup is " + cup + " percent full.");
+			cup++;
+		}
+		print("The cup is full.");
+
+		while(boat > 0){
+			print("The boat is " + boat + " percent visible.");
+			boat--;
+		}
+		print("The boat has sunk.");
+
+		while(gas > 0){
+			print("The gas tank is " + gas + " percent full.");
+			gas--;
+		}
+		print("The car is stuck.");
+
 	}
 	
 	// Update is called once per frame
