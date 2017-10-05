8cfd900e7dd30d6c6f035217ab199ee917b7674e
Unknown <kendrickgines@gmail.com>
2017-10-04 20:10:54 -0600

    Created Void Functions

---
+++ DGM1600_Game/Assets/VoidFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidFunctions : MonoBehaviour {
+	public int laserBlast;
+	// Use this for initialization
+	void Start () {
+		
+		DeathStar(laserBlast);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void DeathStar (int vent) {
+		// Destroys planets.
+		print("That's no moon.  It's a space station.");
+
+		if(vent == 1){
+			print("Porkins misses the mark.");
+		}
+		else if(vent == 2){
+			print("Old dude misses the mark.");
+		}
+		else{
+			print("Luke makes the shot.");
+		}
+	}
+}
