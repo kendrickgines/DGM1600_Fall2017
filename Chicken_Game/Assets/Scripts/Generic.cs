07e225ea80109531916bd731c9458cafa9793e53
Unknown <kendrickgines@gmail.com>
2017-11-08 20:16:16 -0700

    WanderAndGeneric Script
    Added a wander script.  Also includes Generic script from last class

---
+++ Chicken_Game/Assets/Scripts/Generic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generic : MonoBehaviour {
+
+	//position 
+	// public float posX;
+	// public float posY = .5f;
+	// public float posZ;
+	public Vector3 pos;
+	//rotation
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
+	// public float rotW = 1;
+	public Quaternion rot;
+
+	//scale
+	// public float scaleX = 1;
+	// public float scaleY = 1;
+	// public float scaleZ = 1;
+	public Vector3 scale;
+
+	// Use this for initialization
+	void Start () {
+		
+		scale = Vector3.one;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// transform.position = new Vector3(posX, posY, posZ);
+		// transform.rotation = new Quaternion(rotX, rotY, rotZ, rotW);
+		// transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+
+		transform.position = pos;
+		transform.rotation = rot;
+		transform.localScale = scale;
+	}
+}
---
+++ Chicken_Game/Assets/Scripts/Wander.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+
+	// Use this for initialization
+	public float moveSpeed;
+
+	void MoveForward(){
+		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+
+	void Turn(){
+		int randomNum = Random.Range(0,360);
+		transform.Rotate(0,randomNum,0);
+	}
+
+	void TurnAround(){
+		int turnNum = Random.Range(160,180);
+		transform.Rotate(0,turnNum,0);
+	}
+
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.tag == "Ground"){
+			MoveForward();
+		}
+	}
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == "Checkpoint"){
+			TurnAround();
+		}
+	}
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "Wall"){
+			Turn();
+		}
+	}
+
+}
